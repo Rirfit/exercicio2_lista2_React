6cdb7988700b5be30a0299d25bf572550828da0d
Rirfit <lilrirfit@gmail.com>
2024-10-20 10:52:58 -0300

    FC

---
+++ exercicio2/src/components/Texto.jsx
@@ -0,0 +1,8 @@
+function Texto(){
+    return (
+        <>
+        <p>Somente a dor coletiva gera união -Jaja</p>
+        </>
+    )
+
+}export default Texto
--- exercicio2/src/main.jsx
+++ exercicio2/src/main.jsx
@@ -1,10 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
-
+import Texto from './components/Texto'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Texto/>
   </StrictMode>,
 )
